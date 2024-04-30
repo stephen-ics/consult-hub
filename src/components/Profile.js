d226653b4f959b8df63a0e57456653e5009f8d42
stephen-ics <stephenni1234@gmail.com>
2024-04-30 19:04:38 -0400

    basic card component + profile page

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,12 +1,25 @@
-import { Box } from '@chakra-ui/react'
-import React from 'react'
+import { AspectRatio, Box, Card, CardBody, CardFooter, Heading, Image, Text, VStack } from '@chakra-ui/react';
+import React from 'react';
 
 const Profile = () => {
   return (
-    <Box bg="gray.200" p="20px" border="1px solid">
-      HELLO
-    </Box>
-  )
+    <Card maxW="sm" borderWidth="1px" borderRadius="lg" overflow="hidden">
+      <AspectRatio ratio={4 / 3}>
+        <Image src="https://via.placeholder.com/150" alt="Profile Picture" objectFit="cover" />
+      </AspectRatio>
+      <Box p={2}>
+        <CardBody>
+          <VStack align="stretch">
+            <Heading size="lg">Stephen Ni</Heading>
+            <Text fontSize="lg">This is a sample description ahahah</Text>
+          </VStack>
+        </CardBody>
+        <CardFooter pt='0' pb='2'>
+          <Text fontSize="3xl" fontWeight="bold">From $5</Text>
+        </CardFooter>
+      </Box>
+    </Card>
+  );
 }
 
-export default Profile
+export default Profile;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,32 @@
-import { Container, Heading, SimpleGrid } from '@chakra-ui/react'
-import React from 'react'
-
-import Profile from '../components/Profile'
+import { Box, Container, Heading, SimpleGrid, Text } from '@chakra-ui/react';
+import React from 'react';
+import Profile from '../components/Profile';
 
 const Home = () => {
   return (
-    <SimpleGrid columns={4} spacing={10} minChildWidth={250}>
-      <Profile />
-      <Profile />
-      <Profile />
-      <Profile />
-      <Profile />
-    </SimpleGrid>
-  )
-}
+    <Box>
+      <Box mb={8} textAlign="center">
+        <Heading as="h1" size="xl" mb={6}>
+          Welcome to My Website
+        </Heading>
+        <Text>Explore some profiles below:</Text>
+      </Box>
+      <Container maxW="65%">
+        <Heading mb='7'>Recommendations</Heading>
+        <SimpleGrid columns={[1, 2, 4, 5]} gap={6} minChildWidth="400px">
+          <Profile />
+          <Profile />
+          <Profile />
+          <Profile />
+          <Profile />
+          <Profile />
+          <Profile />
+          <Profile />
+          <Profile />
+        </SimpleGrid>
+      </Container>
+    </Box>
+  );
+};
 
-export default Home
+export default Home;
